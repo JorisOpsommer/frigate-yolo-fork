1b47f68056c1aba75826daf8c9efe3d9ae59ad00
Joris Opsommer <joris.opsommer@gmail.com>
2025-07-08 22:23:29 +0200

    feat(model): init

--- frigate/detectors/plugins/edgetpu_tfl.py
+++ frigate/detectors/plugins/edgetpu_tfl.py
@@ -60,25 +60,42 @@ class EdgeTpuTfl(DetectionApi):
         self.interpreter.set_tensor(self.tensor_input_details[0]["index"], tensor_input)
         self.interpreter.invoke()
 
-        boxes = self.interpreter.tensor(self.tensor_output_details[0]["index"])()[0]
-        class_ids = self.interpreter.tensor(self.tensor_output_details[1]["index"])()[0]
-        scores = self.interpreter.tensor(self.tensor_output_details[2]["index"])()[0]
-        count = int(
-            self.interpreter.tensor(self.tensor_output_details[3]["index"])()[0]
+        raw_scores = self.interpreter.tensor(self.tensor_output_details[0]["index"])()[
+            0
+        ]
+        raw_boxes = self.interpreter.tensor(self.tensor_output_details[1]["index"])()[0]
+        # raw_count = int(
+        #     self.interpreter.tensor(self.tensor_output_details[2]["index"])()[0]
+        # )
+        raw_classes = self.interpreter.tensor(self.tensor_output_details[3]["index"])()[
+            0
+        ]
+
+        # 3 Get quantization params for each tensor
+        scale_s, zp_s = self.tensor_output_details[0]["quantization"]
+        scale_b, zp_b = self.tensor_output_details[1]["quantization"]
+        scale_c, zp_c = self.tensor_output_details[2]["quantization"]
+        scale_cl, zp_cl = self.tensor_output_details[3]["quantization"]
+
+        # 4 Dequantize
+        scores = (raw_scores.astype(np.float32) - zp_s) * scale_s
+        boxes = (raw_boxes.astype(np.float32) - zp_b) * scale_b
+        # count = int(round((raw_count.astype(np.float32)[0] - zp_c) * scale_c))
+        classes = np.round((raw_classes.astype(np.float32) - zp_cl) * scale_cl).astype(
+            int
         )
 
-        detections = np.zeros((20, 6), np.float32)
-
-        for i in range(count):
-            if scores[i] < 0.4 or i == 20:
+        # detections = np.zeros((20, 6), np.float32)
+        detections = np.zeros((20, 6), dtype=np.float32)
+        for i in range(5):
+            if scores[i] < 0.4:
                 break
             detections[i] = [
-                class_ids[i],
+                classes[i],
                 float(scores[i]),
                 boxes[i][0],
                 boxes[i][1],
                 boxes[i][2],
                 boxes[i][3],
             ]
-
         return detections
